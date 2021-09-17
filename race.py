2114de5ed9861a9a79a8e2947f174c9fb8c8264a
Paul <paul@debian.paul>
2021-09-17 15:09:38 +0200

    comments in race

--- race.py
+++ race.py
@@ -19,24 +19,29 @@ class Race:
         The result list is divided into two lists: the list of ranked cars and the list of unranked cars.
         Depending on the grading_unit, the ranked list is sorted by time (grading_unit = 's') or by distance (grading_unit = 'm')
         """
-        rank = 1
+        
         ranked, unranked = [], []
-        for result in results:
+        for result in results: # split results in two list : ranked and if necessary unranked cars
             if isinstance(result[0], OutOfGazError):
                 unranked.append(result)
             else:
                 ranked.append(result)
+    
+        # Sort the two list, if grading_unit is 'm' the distance covered is calculated from the race time and the maximum_speed of the car.
+        # For the unranked car, sort is only done by distance covered.
         ranked.sort(key=None if grading_unit == 's' else lambda car_ranked: car_ranked[0] * car_ranked[1].maximum_speed, reverse=True if grading_unit == 'm' else False)
         unranked.sort(key=lambda car_unranked: car_unranked[0].move_time * car_unranked[1].maximum_speed, reverse=True)
+
+        rank = 1
         if ranked:
             print("=== RANKED ===")
-            for i in range(0, len(ranked), 1):
-                print(f"Rank {rank} -> {'TIME' if grading_unit == 's' else 'DISTANCE'} : {round(ranked[i][0], 3) if grading_unit == 's' else ranked[i][1].maximum_speed} {grading_unit} -> Car : {ranked[i][1].model} - {ranked[i][1].name}")
+            for car in ranked:
+                print(f"Rank {rank} -> {'TIME' if grading_unit == 's' else 'DISTANCE'} : {round(car[1], 3) if grading_unit == 's' else car[1].maximum_speed} {grading_unit} -> Car : {car[1].model} - {car[1].name}")
                 rank += 1
         if unranked:
             print("=== UNRANKED ===")    
-            for i in range(0, len(unranked), 1):
-                print(f"Rank {rank} -> DISTANCE : {round(unranked[i][0].move_time * unranked[i][1].maximum_speed, 3)} m -> Car : {unranked[i][1].model} - {unranked[i][1].name} ")
+            for car in unranked:
+                print(f"Rank {rank} -> DISTANCE : {round(car[0].move_time * car[1].maximum_speed, 3)} m -> Car : {car[1].model} - {car[1].name} ")
                 rank += 1
     
     def full_gas(self) -> None:
